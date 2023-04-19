8d7f4c3039b8d7bd99c62ed52fe563fccb8ac2d4
Jonathan Rodriguez Aralla <jrodriguez.aralla@gmail.com>
2023-04-18 22:35:28 -0300

    modificando función addProducts, se guarda el array de objetos en archivo JSON

--- index.js
+++ index.js
@@ -1,56 +1,91 @@
+//Desafio Clase 4 - Manejo de archivos en Javascript
+
+const fs = require("fs");
+
 class ProductManager {
-  static id = 0; // ID que será vistos por todas las instancias
-  constructor() {
-    this.products = [];
-  }
-  addProducts(title, description, price, thumbnail, code, stock) {
-    let newProduct = {
-      id: ProductManager.id,
-      title: title,
-      description: description,
-      price: price,
-      thumbnail: thumbnail,
-      code: code,
-      stock: stock,
-    };
+	static id = 0; // ID que será vistos por todas las instancias
 
-    let codes = this.products.map((cod) => cod.code); // me quedo con todos los códigos del array productos
-    if (!codes.includes(code)) { //evaluo si el codigo del nuevo producto no existe
-      this.products.push(newProduct);
-      ProductManager.id += 1;
-    } else {
-      return console.log('Error: product already exist');
-    }
-  }
+	constructor(myPath) {
+		this.products = [];
+		this.path = myPath;
+	}
 
-  getProducts() {
-    return this.products;
-  }
+	async addProducts(title, description, price, thumbnail, code, stock) {
+		let newProduct = {
+			id: ProductManager.id,
+			title: title,
+			description: description,
+			price: price,
+			thumbnail: thumbnail,
+			code: code,
+			stock: stock,
+		};
 
-  getProductsById(idBuscado) {
-    const result = this.products.find((element) => element.id === idBuscado); // busco el elemento que coincida con el ID indicado
-    if (result) { // Si tengo un resultado lo retorno, sino devuelvo error
-      return result;
-    } else { 
-      return console.log('Error: Product not found');
-    }
-  }
+		let codes = this.products.map((cod) => cod.code); // me quedo con todos los códigos del array productos
+		if (!codes.includes(code)) {
+			//evaluo si el codigo del nuevo producto no existe
+			this.products.push(newProduct);
+			ProductManager.id += 1; //incremento contador ID
+
+			await fs.promises.writeFile(
+				//Escribo el file utilizando promesas y esperando a que se cumpla la misma
+				`${this.path}`,
+				JSON.stringify(this.products)
+			);
+		} else {
+			return console.log("Error: product already exist");
+		}
+	}
+
+	getProducts() {
+		return this.products;
+	}
+
+	getProductsById(idBuscado) {
+		const result = this.products.find(
+			(element) => element.id === idBuscado
+		); // busco el elemento que coincida con el ID indicado
+		if (result) {
+			// Si tengo un resultado lo retorno, sino devuelvo error
+			return result;
+		} else {
+			return console.log("Error: Product not found");
+		}
+	}
 }
 
 //Genero nueva instancia de ProductManager
-let ProductList = new ProductManager();
+let ProductList = new ProductManager("./ProductManager.json");
 
 //Solicito ver el listado de productos, en este caso se devuelve el array vacio
 console.log(ProductList.getProducts());
 
 //Agrego un producto
 ProductList.addProducts(
-  'producto prueba',
-  'este es un producto de prueba',
-  200,
-  'sin imagen',
-  'abc123',
-  5
+	"producto prueba",
+	"este es un producto de prueba",
+	200,
+	"sin imagen",
+	"abc123",
+	5
+);
+
+ProductList.addProducts(
+	"producto prueba2",
+	"este es un producto de prueba2",
+	300,
+	"sin imagen",
+	"abc",
+	10
+);
+
+ProductList.addProducts(
+	"producto prueba3",
+	"este es un producto de prueba3",
+	100,
+	"sin imagen",
+	"123",
+	8
 );
 
 //Solicito ver el listado de productos, en este caso se devuelve el array con el producto ingresado
@@ -58,12 +93,12 @@ console.log(ProductList.getProducts());
 
 //Intento agregar el mismo producto nuevamente, obtengo un Error
 ProductList.addProducts(
-  'producto prueba',
-  'este es un producto de prueba',
-  200,
-  'sin imagen',
-  'abc123',
-  5
+	"producto prueba",
+	"este es un producto de prueba",
+	200,
+	"sin imagen",
+	"abc123",
+	5
 );
 
 //Solicito mostrar el producto con ID 0, y muestra el mismo
