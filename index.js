0af7b3be358d5f69093d6f3f6264af3b0829a6e0
Jonathan Rodriguez Aralla <jrodriguez.aralla@gmail.com>
2023-04-19 21:28:54 -0300

    se agrega función UpdateProduct para actualizar un producto indicando ID

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ class ProductManager {
 		this.path = myPath;
 	}
 
+	//Método para agregar productos al archivo
 	async addProducts(title, description, price, thumbnail, code, stock) {
 		let newProduct = {
 			id: ProductManager.id,
@@ -22,23 +23,26 @@ class ProductManager {
 		};
 
 		let codes = this.products.map((cod) => cod.code); // me quedo con todos los códigos del array productos
+
+		//evaluo si el codigo del nuevo producto no existe
 		if (!codes.includes(code)) {
-			//evaluo si el codigo del nuevo producto no existe
 			this.products.push(newProduct);
 			ProductManager.id += 1; //incremento contador ID
 
-			//Escribo el file utilizando promesas y esperando a que se cumpla la misma
+			//Si no existe, Escribo el file utilizando promesas y esperando a que se cumpla la misma
 			await fs.promises.writeFile(`${this.path}`, JSON.stringify(this.products));
 		} else {
 			return console.log("Error: product already exist");
 		}
 	}
 
+	//Método para adquirir el listado de productos desde el archivo.
 	async getProducts() {
 		const actualProducts = await fs.promises.readFile(`${this.path}`, "utf-8");
 		return JSON.parse(actualProducts);
 	}
 
+	//Método para adquirir un producto especifico por ID
 	async getProductsById(idBuscado) {
 		const productList = await this.getProducts();
 		const result = productList.find((element) => element.id === idBuscado); // busco el elemento que coincida con el ID indicado
@@ -50,6 +54,25 @@ class ProductManager {
 			return "Error: Product not found";
 		}
 	}
+
+	//Método para actualizar producto
+	async updateProduct(idBuscado, productUpdated) {
+		const productList = await this.getProducts();
+		productList.map((product) => {
+			if (product.id === idBuscado) {
+				product.title = productUpdated.title;
+				product.description = productUpdated.description;
+				product.price = productUpdated.price;
+				product.thumbnail = productUpdated.thumbnail;
+				product.code = productUpdated.code;
+				product.stock = productUpdated.stock;
+			}
+			return product;
+		});
+
+		await fs.promises.writeFile(`${this.path}`, JSON.stringify(productList));
+		return productList;
+	}
 }
 
 //Genero nueva instancia de ProductManager
@@ -57,21 +80,31 @@ let ProductList = new ProductManager("./ProductManager.json");
 
 //Solicito ver el listado de productos, en este caso se devuelve el array vacio
 const test = async () => {
+	const newProd = {
+		title: "producto prueba2",
+		description: "este es un producto de prueba2",
+		price: 20,
+		thumbnail: "sin imagen 2",
+		code: "asdasd",
+		stock: 25,
+	};
+
 	try {
 		//Agrego un producto
 		await ProductList.addProducts("producto prueba", "este es un producto de prueba", 200, "sin imagen", "abc123", 5);
 
+		console.log(await ProductList.updateProduct(0, newProd));
 		//Solicito ver el listado de productos, en este caso se devuelve el array con el producto ingresado
-		console.log(await ProductList.getProducts());
+		//console.log(await ProductList.getProducts());
 
 		//Intento agregar el mismo producto nuevamente, obtengo un Error
-		await ProductList.addProducts("producto prueba", "este es un producto de prueba", 200, "sin imagen", "abc123", 5);
+		//await ProductList.addProducts("producto prueba", "este es un producto de prueba", 200, "sin imagen", "abc123", 5);
 
 		//Solicito mostrar el producto con ID 0, y muestra el mismo
-		console.log(await ProductList.getProductsById(0));
+		//console.log(await ProductList.getProductsById(0));
 
 		//Solicito mostrar el producto con ID 10, y muestra Error porque el mismo no existe
-		console.log(await ProductList.getProductsById(0));
+		//console.log(await ProductList.getProductsById(0));
 	} catch (error) {
 		console.log("Error en el test");
 	}
