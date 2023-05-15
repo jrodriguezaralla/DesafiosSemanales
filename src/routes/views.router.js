5835379c7e21da2d536e12acfe7502a86d839304
Jonathan Rodriguez Aralla <jrodriguez.aralla@gmail.com>
2023-05-15 15:03:45 -0300

    agregando endpoint / el cual devuelve una pagina home con el listado de todos los productos

--- src/routes/views.router.js
+++ src/routes/views.router.js
@@ -1,17 +1,18 @@
 import { Router } from 'express';
+import ProductManager from '../models/ProductManager.js';
 
 const viewsRouter = Router();
+
+//Instancio una nueva clase de Product Manager con el archivo ya creado
+const ProductList = new ProductManager('./productos.json');
 
 //Endpoint que muestra un usuario
 viewsRouter.get('/', async (req, res) => {
 	try {
-		let testUser = {
-			name: 'Pedro',
-			last_name: 'Martinez',
-		};
-		res.render('index', {
-			//name: testUser.name,
-			//style: 'index.css',
+		let products = await ProductList.getProducts();
+		res.render('home', {
+			products,
+			style: 'index.css',
 		});
 	} catch (error) {
 		res.status(400).send(error);
