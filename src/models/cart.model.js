67ae6f0de7e583d1be3d43a6592444262b7e2828
Jonathan Rodriguez Aralla <jrodriguez.aralla@gmail.com>
2023-05-31 21:04:35 -0300

    finalizando metodo para agregar productos al carrito

--- src/models/cart.model.js
+++ src/models/cart.model.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import { ProductModel } from './product.model.js';
 
 const cartSchema = new mongoose.Schema({
 	products: [
--- src/service/Cart.service.js
+++ src/service/Cart.service.js
@@ -30,43 +30,19 @@ class CartService {
 			product: productId,
 			quantity: 1,
 		};
+		//console.log(cartId);
+		const cart = await this.model.findById(cartId); //me quedo con el carrito a modificar
+		const prod = await cart.products.id(productId); //dentro del carrito busco el id de mongo asignado al producto
+		if (prod) {
+			//Si existe sumo una unidad
+			prod.quantity += 1;
+		} else {
+			//Si no axiste el producto lo agrego
+			cart.products.push(newProduct);
+		}
 
-		const cart = await this.model.findById(cartId);
-		console.log(cart);
-		cart.products.push(newProduct);
-		console.log(cart.products);
-		await cart.save();
-		return cart;
-		/*
-		let existe = false; // flag para indicar si el producto ya existe en el carrito
-		const newProduct = {
-			product: productId,
-			quantity: 1,
-		};
-
-		const result = this.model.findById({ _id: cartId }); // busco el elemento que coincida con el ID indicado
-		if ((await result).length == 0) return { error: `Error: Cart ID=${cartId} not found` }; //si no lo encuentra retorno error
-
-		productCarts.map((cart) => {
-			//recorro el array buscando el ID del carrito indicado
-			if (cart.id === cartId) {
-				cart.products.map((prod) => {
-					//por cada carrito busco el producto indicado
-					if (prod.product === productId) {
-						//Si existe sumo una unidad
-						prod.quantity += 1;
-						existe = true; //enciendo flag
-					}
-				});
-				if (!existe) {
-					//Si no axiste el producto lo agrego
-					cart.products.push(newProduct);
-					existe = false;
-				}
-			}
-		});
-		await fs.promises.writeFile(`${this.path}`, JSON.stringify(productCarts));
-		return { status: 'sucess', message: `product ID=${productId} added to cart ID=${cartId}` };*/
+		await cart.save(); //guardo cambios
+		return { status: 'sucess', message: `product ID=${productId} added to cart ID=${cartId}` }; // retorno el carrito con el producto agregado
 	}
 }
 
