995694ef16c413cccc1704c12b5e23d0fc64fc98
Jonathan Rodriguez Aralla <jrodriguez.aralla@gmail.com>
2023-06-11 23:39:17 -0300

    agregando Offcanvas de bootstrap para mostrar productos en el carrito

--- src/public/js/products.js
+++ src/public/js/products.js
@@ -1,6 +1,7 @@
-let carrito = JSON.parse(localStorage.getItem('carrito')) || []; // si tengo guardado un carrito en Local Storage lo asigno a carrito, sino le asigno un array vacio
 let btnAgregarCarrito = document.querySelectorAll('.btnAgregarCarrito'); // NodeList = [button#1, button#2 .... , button#n]
 const cartId = '6476a662e14fcee005c06b2c';
+const contenedorCarrito = document.querySelector('#contenedorCarrito'); // contenedor donde se muestran los productos en la venta de carrito
+const btnCarrito = document.querySelector('#btnCarrito'); // al presionar sobre el logo del carrito llamo a la función para mostrar los elementos en el carrito
 
 btnAgregarCarrito.forEach((el) => {
 	el.addEventListener('click', async (e) => {
@@ -11,10 +12,60 @@ btnAgregarCarrito.forEach((el) => {
 			},
 		})
 			.then(() => {
-				alert('Se agrego producto al carrito');
+				const Toast = Swal.mixin({
+					toast: true,
+					position: 'top-end',
+					showConfirmButton: false,
+					timer: 3000,
+					timerProgressBar: true,
+					didOpen: (toast) => {
+						toast.addEventListener('mouseenter', Swal.stopTimer);
+						toast.addEventListener('mouseleave', Swal.resumeTimer);
+					},
+				});
+
+				Toast.fire({
+					icon: 'success',
+					title: 'Producto agregado al carrito!',
+				});
 			})
 			.catch((err) => {
 				alert(`Error al agregar producto al carrito ${err}`);
 			});
 	});
 });
+
+btnCarrito.addEventListener('click', async () => {
+	await fetch(`/api/carts/${cartId}`)
+		.then((res) => res.json())
+		.then((data) => {
+			agregarElmentoCarrito(data);
+		});
+});
+
+//Función para mostrar en pantalla los productos que van siendo agregados al carrito
+function agregarElmentoCarrito(dato) {
+	contenedorCarrito.innerText = ''; // Borro leyenda "Agregue productos al carrito"
+	dato.forEach((elemento) => {
+		let div = document.createElement('div');
+		div.className = 'card mb-2';
+		div.style = 'height: 150px';
+		div.innerHTML = `
+            <div class="row w-100 align-items-center ms-1" >
+                <div class="col-md-4 ms-auto d-flex justify-content-center">
+                    <img src="${elemento.product.thumbnail}" class="img-fluid rounded-start px-1 " alt="img_${elemento.product.code}">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body p-0 px-1 position-relative">
+                        <h5 class="card-title m-0 fs-6 pe-4">${elemento.product.title} - ${elemento.product.code}</h5>
+                        <p class="card-text m-0 fs-6 pe-3">Descripción: ${elemento.product.description}</p>
+                        <p class="card-text m-0 fs-6 pe-3">Cantidad: ${elemento.quantity}</p>
+                        <p class="fw-bold fst-italic fs-5 m-0">Precio: ${elemento.product.price} USD</p>
+
+                    </div>
+                </div>
+            </div>
+            `;
+		contenedorCarrito.append(div);
+	});
+}
