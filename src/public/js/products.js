379dc46483ccd9093891fd6ce945b859163e9f1d
Jonathan Rodriguez Aralla <jrodriguez.aralla@gmail.com>
2023-06-11 21:53:45 -0300

    agregando populate al traer el array de productos

--- src/public/js/products.js
+++ src/public/js/products.js
@@ -4,8 +4,11 @@ const cartId = '6476a662e14fcee005c06b2c';
 
 btnAgregarCarrito.forEach((el) => {
 	el.addEventListener('click', async (e) => {
-		fetch(`/api/carts/${cartId}/product/${e.target.id}`, {
+		await fetch(`/api/carts/${cartId}/product/${e.target.id}`, {
 			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
 		})
 			.then(() => {
 				alert('Se agrego producto al carrito');
