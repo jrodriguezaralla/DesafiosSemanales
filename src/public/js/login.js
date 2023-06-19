df1a60e57af00b0953f88cfaecf374d588dd2a5e
Jonathan Rodriguez Aralla <jrodriguez.aralla@gmail.com>
2023-06-19 00:59:28 -0300

    arreglando error ortografico que no redirijia login a pantalla de productos.

--- src/public/js/login.js
+++ src/public/js/login.js
@@ -28,7 +28,7 @@ btnLogin.addEventListener('click', async () => {
 	})
 		.then((response) => response.json())
 		.then((data) => {
-			if (data.status === 'sucess') {
+			if (data.status === 'success') {
 				//Si tengo sucess en la respuesta redirijo a la vista productos
 				window.location.replace('/products');
 			}
