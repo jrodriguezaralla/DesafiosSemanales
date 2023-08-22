f01472d43acd746e9a1d83c573b503d8a704af33
Jonathan Rodriguez Aralla <jrodriguez.aralla@gmail.com>
2023-08-21 23:43:29 -0300

    agregando error cutomizado

--- src/middleware/jwtrestorepass.middleware.js
+++ src/middleware/jwtrestorepass.middleware.js
@@ -1,13 +1,27 @@
 import jwt from 'jsonwebtoken';
 import environment from '../config/environment.js';
+import CustomError from '../tools/CustomErrors.js';
+import EErrors from '../tools/EErrors.js';
 
 const validateTokenRestorePass = (req, res, next) => {
 	const { token } = req.query;
-	if (!token) es.json({ status: 'error', message: 'access denied' });
+	if (!token) {
+		CustomError.createError({
+			name: 'access prohibited',
+			cause: 'access denied',
+			message: 'Error trying access to the indicated route',
+			code: EErrors.ROUTING_ERROR,
+		});
+	}
 
 	jwt.verify(token, environment.restorepasskey, (err, user) => {
 		if (err) {
-			res.json({ status: 'error', message: 'access denied, token expired or incorrect' });
+			CustomError.createError({
+				name: 'token expired or incorrect',
+				cause: 'access denied, token expired or incorrect',
+				message: 'Error trying access to the indicated route',
+				code: EErrors.ROUTING_ERROR,
+			});
 		} else {
 			next();
 		}
