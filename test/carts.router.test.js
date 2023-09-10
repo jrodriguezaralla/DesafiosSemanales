4aa47737f45dc240d98917f1413caf35ef5ceb0a
Jonathan Rodriguez Aralla <jrodriguez.aralla@gmail.com>
2023-09-10 07:06:33 -0300

    modificando variable de logueo con admin utilizando las variables de entrono

--- test/carts.router.test.js
+++ test/carts.router.test.js
@@ -10,7 +10,7 @@ describe('Test de integracion - Productos', () => {
 	let productId;
 
 	it('Verificando listado de productos', async () => {
-		const response = await request.get('/api/products')
+		const response = await request.get('/api/products');
 		expect(response.status).to.equal(200);
 		expect(response._body.status).to.be.ok.and.equal('success');
 	});
@@ -28,13 +28,13 @@ describe('Test de integracion - Productos', () => {
 		};
 
 		const user = {
-			username: environment.adminName,
-			password: environment.adminPassword,
+			username: 'adminCoder@coder.com',
+			password: 'adminCod3r123',
 		};
 		const response = await request.post('/api/users/auth').send(user);
 		authTokenCookie = response.headers['set-cookie'];
-		const {_body} = await request.post('/api/products').set('Cookie',authTokenCookie).send(product);
-		productId= _body.payload._id;
+		const { _body } = await request.post('/api/products').set('Cookie', authTokenCookie).send(product);
+		productId = _body.payload._id;
 		expect(_body.status).to.be.ok.and.equal('success');
 		expect(_body.payload).to.have.property('_id');
 	});
@@ -61,7 +61,7 @@ describe('Test de integracion - Productos', () => {
 	});
 
 	after(async () => {
-		const { _body } = await request.delete(`/api/products/${productId}`).set('Cookie',authTokenCookie);
+		const { _body } = await request.delete(`/api/products/${productId}`).set('Cookie', authTokenCookie);
 		expect(_body.status).to.be.ok.and.equal('success');
 
 		const response = await request.post(`/api/users/logout`);
